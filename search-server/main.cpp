8e9ca34bbbea958d7ad6aae9bd072e19726c3717
Tatyana Rusakova <t.rusakova@orientgroup.uz>
2023-07-07 23:17:57 +0500

    sprint 1 theme 1 lesson 8 version 1

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,23 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+	int incl_3 = 0;
+	for (int i = 1; i <= 1000; ++i) {
+		string num = to_string(i);
+		bool flag = false;
+		for (const char& c : num) {
+			if (c == '3') {
+				flag = true;
+			}
+		}
+		if (flag) {
+			++incl_3;
+		}
+	}
+	cout << incl_3 << endl;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
